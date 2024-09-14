f8b313894e06d1033171e14a2dfbf034ae65e430
AAnet07 <alexiavillnetp.2020@gmail.com>
2024-09-13 23:54:19 -0500

    avance de js

--- assets/js/detalles-per.js
+++ assets/js/detalles-per.js
@@ -1,47 +1,57 @@
 $(document).ready(function () {
     const data = JSON.parse(localStorage.getItem("personaje-data"));
 
-    // Mi función para mostrar los pokemones
+    // Mi función para mostrar los personajes
     let personajesCard = $("<div></div>").addClass("personajes-card");
-    // Nombre y numero del pokemon
+    //Contenedor
+    let nameContainer = $("<div></div>").addClass("name-container");
+    // Nombre y ki del personaje
     let name = $("<h1>data.name</h1>").text(
         data.name.toUpperCase()).addClass("personajes-name");
-
     let ki = $("<p></p>").text(data.ki);
     
-    let nameContainer = $("<div></div>").addClass("name-container");
     nameContainer.append(name, ki);
 
     //Mostrar imagen 
     let img = $("<img />").attr("src", data.image);
 
-    // ki, race, gender, originPlanet.name
     //Mostrar race, gender, planet
     let maxKi = $(`<p>MaxKi: ${data.maxKi}</p>`).addClass("maxki");
     let race = $("<p></p>").text(data.race.toUpperCase()).addClass("race");
     let gender = $("<p></p>").text(data.gender.toUpperCase()).addClass("gender");
     let description = $("<p></p>").text(data.description).addClass("description");
     let infoCard = $("<div></div>").addClass("info-card");
-    infoCard.append(maxKi, race, gender, description);
-
-    //Contenedor planeta
-    let planetContainer = $("<div></div>").addClass("planet-container");
-    let namePlaneta = $("<p></p>").text(data.originPlanet);
-    console.log(namePlaneta);
+    
+    // Mi función para mostrar el planet del personaje
+    let planetCard = $("<div></div>").addClass("planet-card");
+    //Info
+    let namePlaneta = $("<p></p>").text(data.originPlanet.name.toUpperCase());
     let planetImg = $("<img />").attr("src", data.originPlanet.image);
     let isDestroyed = $("<p></p>").text(data.originPlanet.isDestroyed);
     let planetDescription = $("<p></p>").text(data.originPlanet.description);
-    planetContainer.append(namePlaneta, planetImg, isDestroyed, planetDescription);
-    console.log(planetContainer);
     
-    let planet = $("<p></p>").text(data.originPlanet.name.toUpperCase()).addClass("planet");
-
-
+    // Mi función para mostrar el transformaciones del personaje
+    let transforCard = $("<div></div>").addClass("transfor-card");
+    //Info
+    for (let transformations of data.transformations) {
+          
+        //Informacion de las trasformaciones del personaje
+        let nameTrasfor = $(`<p></p>`).text(transformations.name.toUpperCase()).addClass("name-transfor");
+        let idTrasfor = $(`<p></p>`).text(`#${transformations.id}`).addClass("idTrasfor");
+        let imgTrasfor = $(`<img />`).attr("src", transformations.image).addClass("name-transfor");
+        let kiTrasfor = $(`<p></p>`).text(character.ki).addClass("ki-trasfor");
+        transforCard.append(nameTrasfor, imgTrasfor, idTrasfor, kiTrasfor);
+    }
 
     //Agregamos los elementos al card
     personajesCard.append(nameContainer, img, infoCard);
+    infoCard.append(maxKi, race, gender, description);
+    planetCard.append(namePlaneta, planetImg, isDestroyed, planetDescription);
+
     //Agregamos la card al contendor 
     $("#personajes-container").append(personajesCard);
+    $("#personajes-container").append(planetCard);
+    $("#personajes-container").append(transforCard);
 
 });
 
--- assets/js/detalles-pla.js
+++ assets/js/detalles-pla.js
@@ -4,27 +4,44 @@ $(document).ready(function () {
 
     let planetCard = $("<div></div>").addClass("planet-card");
         // Mostrar nombre del planeta y su id
+        let nameContainer = $("<div></div>").addClass("name-container");
+
         let name = $("<h1></h1>").text(
             data.name.toUpperCase()
         ).addClass("planet-name");
 
         let num = $("<p></p>").text(`#${data.id}`).addClass("ki");
-
-        let nameContainer = $("<div></div>").addClass("name-container");
         nameContainer.append(name, num);
 
         //Mostrar imagen del planeta
         let img = $("<img />").attr("src", data.image);
 
+        //Contenedor de la informacion principal
+        let infoCard = $("<div></div>").addClass("info-card");
+        infoCard.append(isDestroyed, description);
+
         //Mostrar isDestroyed y deletedAt
         let isDestroyed = $(`<p>Is destroyed? ${data.isDestroyed}</p>`).addClass("isdestroyed");
+        let description = $(`<p>Description: ${data.description}</p>`).addClass("description");
         
-        let infoCard = $("<div></div>").addClass("info-card");
-        infoCard.append(isDestroyed);
+    
+        let charactersContainer = $("<div></div>").addClass("characters-container");
+
+        for (let character of data.characters) {
+          
+            //Informacion de los personajes del planeta
+            let img = $(`<img />`).attr("src", character.image).addClass("name-char");
+            let name = $(`<p></p>`).text(character.name).addClass("name-char");
+            let maxKi = $(`<p></p>`).text(character.maxKi).addClass("maxKi-char");
+            let race = $(`<p></p>`).text(character.race).addClass("race");
+            let gender = $(`<p></p>`).text(character.gender).addClass("gender");
+            let description = $(`<p></p>`).text(character.description).addClass("description");
+            let affiliation = $(`<p></p>`).text(character.affiliation).addClass("affiliation");
+            charactersContainer.append(name, img, maxKi, race, gender, description, affiliation);
+        }
 
         //Agregamos los elementos al card
-        planetCard.append(nameContainer, img, infoCard);
+        planetCard.append(nameContainer, img, infoCard, charactersContainer);
         //Agregamos la card al contendor 
         $("#planetas-container").append(planetCard);
 });
-  
